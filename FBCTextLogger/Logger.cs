26f1152c0e4f91d48fdfdab51ace2ed1b2c44e01
Fatih BAHCECI <fatih_bah@hotmail.com>
2020-09-09 17:29:24 +0300

    First Files

---
+++ FBCTextLogger/Logger.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace FBCTextLogger
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Logger : IDisposable
+    {
+        private enum ELogAction
+        {
+            InitChannel, DisposeChannel, WriteLog
+        }
+
+        private class LogAction
+        {
+            public ELogAction Action { get; set; }
+            public string ChannelName { get; set; }
+            public string Log { get; set; }
+            public object[] prms { get; set; }
+
+            public LoggerOptions Options { get; set; }
+        }
+
+        private static ConcurrentDictionary<string, Logger> channels = new ConcurrentDictionary<string, Logger>();
+        private static ConcurrentQueue<LogAction> queue = new ConcurrentQueue<LogAction>();
+        private volatile static bool isConsuming = false;
+        /// <summary>
+        /// Write log to main channel
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="ss"></param>
+        public static void Log(string s, params object[] ss)
+        {
+            LogC("", s, ss);
+        }
+        /// <summary>
+        /// Write log to channel by named as.
+        /// </summary>
+        /// <param name="channelName"></param>
+        /// <param name="s"></param>
+        /// <param name="ss"></param>
+        public static void LogC(string channelName, string s, params object[] ss)
+        {
+            queue.Enqueue(new LogAction()
+            {
+                Action = ELogAction.WriteLog,
+                ChannelName = channelName,
+                Log = s,
+                prms = ss
+            });
+            consumeWorks();
+        }
+
+        private static void consumeWorks()
+        {
+            if (!isConsuming)
+            {
+                new Thread(x => { doConsumeWorks(); }).Start();
+            }
+        }
+
+        private static void InitChannelInternal(LoggerOptions opt)
+        {
+            if (channels.ContainsKey(opt.ChannelName))
+            {
+                //if is main channel. Don't touch! (Main Channel name = "")
+                if (!string.IsNullOrEmpty(opt.ChannelName))
+                {
+                    try
+                    {
+                        DisposeChannel(opt.ChannelName);
+                    }
+                    catch { }
+                    channels[opt.ChannelName] = new Logger(opt);
+
+                }
+            }
+            else
+            {
+                channels[opt.ChannelName ?? ""] = new Logger(opt);
+            }
+        }
+
+        private static void DisposeChannelInternal(string channelName)
+        {
+            //if is main channel dont dispose it.  (Main Channel name = "")
+            if (!string.IsNullOrEmpty(channelName) && channels.ContainsKey(channelName))
+            {
+                var channel = channels[channelName];
+                channel.Dispose();
+                channels.TryRemove(channelName, out channel);
+            }
+        }
+        /// <summary>
+        /// Wait until all task(s) consumed
+        /// </summary>
+        /// <param name="timeout">Milliseconds</param>
+        public static void WaitConsumer(int timeout = -1)
+        {
+            if (timeout > 0)
+            {
+                var s = DateTime.Now;
+                while (isConsuming && (DateTime.Now - s).TotalMilliseconds < timeout)
+                {
+                    Thread.Sleep(333);
+                }
+
+            }
+            else
+            {
+                while (isConsuming)
+                {
+                    Thread.Sleep(333);
+                }
+            }
+        }
+
+
+        private static void doConsumeWorks()
+        {
+            if (!isConsuming)
+            {
+                try
+                {
+                    isConsuming = true;
+                    LogAction act = null;
+                    while (queue.Count > 0)
+                    {
+
+                        if (queue.TryDequeue(out act))
+                        {
+                            try
+                            {
+                                switch (act.Action)
+                                {
+                                    case ELogAction.InitChannel:
+                                        LoggerOptions opt = act.Options ?? new LoggerOptions(!string.IsNullOrEmpty(act.ChannelName) ? act.ChannelName : "");
+                                        InitChannelInternal(opt);
+                                        break;
+                                    case ELogAction.DisposeChannel:
+                                        DisposeChannelInternal(act.ChannelName);
+                                        break;
+                                    case ELogAction.WriteLog:
+                                        string c = act.ChannelName ?? "";
+                                        if (!channels.ContainsKey(c))
+                                        {
+                                            //if this isn't main channel
+                                            if (!string.IsNullOrEmpty(c))
+                                            {
+                                                Logger.Log("Automatically creating new channel named: " + c + ".");
+
+                                            } //else don't log anything
+                                            channels[c] = new Logger(new LoggerOptions(c, true, "{0} -> "));
+                                        }
+                                        channels[c].WriteLog(act.Log, act.prms);
+                                        break;
+                                }
+                            }
+                            catch
+                            {
+
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    isConsuming = false;
+                }
+            }
+        }
+        public static void InitChannel(LoggerOptions opt)
+        {
+            queue.Enqueue(new LogAction()
+            {
+                Action = ELogAction.InitChannel,
+                ChannelName = opt != null ? opt.ChannelName : "",
+                Log = null,
+                prms = null,
+                Options = opt
+            });
+            consumeWorks();
+        }
+
+        public static void DisposeChannel(string channelName)
+        {
+            queue.Enqueue(new LogAction()
+            {
+                Action = ELogAction.DisposeChannel,
+                ChannelName = channelName,
+                Log = null,
+                prms = null
+            });
+            consumeWorks();
+        }
+
+        public Logger(LoggerOptions options)
+        {
+            this.opt = options;
+        }
+
+        private TextWriter writer;
+        private object writerLock = new object();
+        //private static ConcurrentQueue<string> lines;
+        //private static volatile bool isConsuming = false;
+        //private static void consumeLines()
+        //{
+
+        //}
+        private void createNewWriter()
+        {
+            totalBytesWritten = 0;
+            if (writer != null)
+            {
+                try { writer.Flush(); } catch { }
+                try { writer.Close(); } catch { }
+                writer = null;
+            }
+            var prc = Process.GetCurrentProcess();
+            Log("App Name: {0}, Process Name: {1}", Path.GetFileName(prc.MainModule.FileName), prc.ProcessName);
+            string fullPath = prc.MainModule.FileName;
+            ////18:30:37 - 838->App Name: post - commit.exe, Process Name: post - commit
+            ////18:30:37 - 839->Full path::C:\NanodemsV2\NDIS\trunk\ExternalIntegrations\SVN_TEAMS_INTEGRATION\PostHookCommit\bin\post - commit.exe
+
+            //string fullPath = Assembly.GetExecutingAssembly().Location;
+            string workingPath = (opt.LogDirectoryPath ?? "").Trim();
+            if (string.IsNullOrEmpty(workingPath))
+            {
+                workingPath = Path.GetDirectoryName(fullPath);
+            }
+            string logFilePath = opt.LogFileNamePrefix;
+            if (string.IsNullOrEmpty(logFilePath))
+            {
+                logFilePath = Path.GetFileNameWithoutExtension(fullPath);
+            }
+            if (!Directory.Exists(workingPath))
+            {
+                Directory.CreateDirectory(workingPath);
+            }
+            string suffix = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss-fff");
+            logFilePath = Path.Combine(workingPath, $"{logFilePath}_{(string.IsNullOrEmpty(opt.ChannelName) ? "" : opt.ChannelName + "_")}{suffix}.log");
+            writer = new StreamWriter(logFilePath, true);
+        }
+
+        private volatile int totalBytesWritten = 0;
+        private LoggerOptions opt;
+
+        private bool isNewLogFileRequired()
+        {
+            //100 MB
+            return writer == null || totalBytesWritten > (1024 * 1024 * 100);
+        }
+
+        public static string ConvertStringFor(string s, LoggerOptions opt, params object[] prms)
+        {
+            if (prms != null && prms.Length > 0)
+            {
+                try { s = String.Format(s, prms); } catch { }
+            }
+            if (opt != null)
+            {
+                if (opt.AddDateTimeStampEachLog)
+                {
+                    if (string.IsNullOrEmpty(opt.DateTimeStampFormat))
+                    {
+                        s = DateTime.Now + s;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            s = string.Format(opt.DateTimeStampFormat, DateTime.Now) + s;
+                        }
+                        catch
+                        {
+                            s = DateTime.Now + s;
+                        }
+                    }
+                }
+            }
+            return s;
+        }
+        public void WriteLog(string s, params object[] prms)
+        {
+
+            lock (writerLock)
+            {
+                if (isNewLogFileRequired())
+                {
+                    createNewWriter();
+                }
+                s = ConvertStringFor(s, opt, prms);
+                writer.WriteLine(s);
+                totalBytesWritten += s != null ? s.Length : 0;
+                writer.Flush();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (writer != null)
+            {
+                try { writer.Flush(); } catch { }
+                try { writer.Close(); } catch { }
+                writer = null;
+            }
+        }
+    }
+}
+/*
+         public static void Test()
+        {
+
+            Logger.InitChannel(new LoggerOptions("MyPrivateChannel", true, "<<<<<{0}>>>>>:"));
+            Logger.InitChannel(new LoggerOptions("MyPrivateChannel2", false, null));
+            new Thread(x =>
+            {
+                for (; ; )
+                {
+                    Logger.Log("This is a default log");
+                    Thread.Sleep(50);
+                }
+            }).Start();
+            new Thread(x =>
+            {
+                for (; ; )
+                {
+                    Logger.LogC("AutoCreated", "This is  a log with not manuel initialized channel");
+                    Thread.Sleep(150);
+                }
+            }).Start();
+            new Thread(x =>
+            {
+                for (; ; )
+                {
+                    Logger.LogC("MyPrivateChannel", "This is  a log with manuel initialized channel named: MyPrivateChannel");
+                    Thread.Sleep(250);
+                }
+            }).Start();
+            new Thread(x =>
+            {
+                for (; ; )
+                {
+                    Logger.LogC("MyPrivateChannel2", "This is  a log with manuel initialized channel named: MyPrivateChannel2");
+                    Thread.Sleep(350);
+                }
+            }).Start();
+
+            new Thread(x =>
+            {
+                for (; ; )
+                {
+                    Logger.LogC("AngryChannel", "This is  a log with manuel initialized channel named: AngryChannel");
+                    Thread.Sleep(1);
+                }
+            }).Start();
+        }
+     */
---
+++ FBCTextLogger/LoggerOptions.cs
@@ -0,0 +1,55 @@
+﻿namespace FBCTextLogger
+{
+    public class LoggerOptions
+    {
+        private string dkfjsdlk_hndjkghnfjkxhgn_jkhg;
+        /// <summary>
+        /// Unique key
+        /// </summary>
+        public string ChannelName
+        {
+            get
+            {
+                return dkfjsdlk_hndjkghnfjkxhgn_jkhg ?? (dkfjsdlk_hndjkghnfjkxhgn_jkhg = "");
+            }
+            set
+            {
+                dkfjsdlk_hndjkghnfjkxhgn_jkhg = value ?? dkfjsdlk_hndjkghnfjkxhgn_jkhg;
+            }
+        }
+        /// <summary>
+        /// Default false
+        /// </summary>
+        public bool AddDateTimeStampEachLog { get; }
+        /// <summary>
+        /// Default "{0}" or null or Empty string
+        /// Example "{0} ->"
+        /// </summary>
+        public string DateTimeStampFormat { get; }
+        /// <summary>
+        /// Default: null -> Current assembly path
+        /// </summary>
+        public string LogDirectoryPath { get; }
+        /// <summary>
+        /// Default: null -> Current assembly name without extension
+        /// </summary>
+        public string LogFileNamePrefix { get; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="channelName">Unique key</param>
+        /// <param name="addDateTimeStampEachLog">Default false</param>
+        /// <param name="dateTimeStampFormat">default null</param>
+        /// <param name="logDirectoryPath">default null:  Current assembly path</param>
+        /// <param name="logFileNamePrefix">default null: Current assembly name without extension</param>
+        public LoggerOptions(string channelName, bool addDateTimeStampEachLog = false, string dateTimeStampFormat = null, string logDirectoryPath = null, string logFileNamePrefix = null)
+        {
+            ChannelName = channelName ?? "";
+            AddDateTimeStampEachLog = addDateTimeStampEachLog;
+            DateTimeStampFormat = dateTimeStampFormat;
+            LogDirectoryPath = logDirectoryPath;
+            LogFileNamePrefix = logFileNamePrefix;
+        }
+    }
+}
